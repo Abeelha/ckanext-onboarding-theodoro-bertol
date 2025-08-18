22c26068d5267af120de11d86a530add6f2cf312
Theo Bertol <dev.theodoro@gmail.com>
2025-08-18 12:36:27 -0300

    Initial extension scaffolding

---
+++ ckanext/onboarding_theodoro_bertol/assets/script.js
@@ -0,0 +1,10 @@
+ckan.module("onboarding_theodoro_bertol-module", function ($, _) {
+  "use strict";
+  return {
+    options: {
+      debug: false,
+    },
+
+    initialize: function () {},
+  };
+});
