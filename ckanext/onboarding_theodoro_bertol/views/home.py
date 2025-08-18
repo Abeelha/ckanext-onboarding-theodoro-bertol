48386d3db7dd5281b5296808e59f66077003d99e
Theo Bertol <dev.theodoro@gmail.com>
2025-08-18 13:10:42 -0300

    Updated route /about

--- ckanext/onboarding_theodoro_bertol/views/home.py
+++ ckanext/onboarding_theodoro_bertol/views/home.py
@@ -6,8 +6,13 @@ log = logging.getLogger(__name__)
 
 home = Blueprint("onboarding_home", __name__)
 
+def about():
+    log.info("/about is being overridden by my plugin!")
+    return base.render('home/about.html', extra_vars={})
+
 def my_new_route():
     log.info("my_new_route view called!")
     return base.render('home/about.html', extra_vars={})
 
+home.add_url_rule("/about", view_func=about)
 home.add_url_rule("/my-new-route", view_func=my_new_route)
