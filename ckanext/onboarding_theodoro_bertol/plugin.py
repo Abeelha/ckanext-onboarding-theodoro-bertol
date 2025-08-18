68af805aaf6bade3183391805ba6f5a5efee3269
Theo Bertol <dev.theodoro@gmail.com>
2025-08-18 13:09:06 -0300

    Added new plugin / route page

--- ckanext/onboarding_theodoro_bertol/plugin.py
+++ ckanext/onboarding_theodoro_bertol/plugin.py
@@ -1,16 +1,23 @@
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
+import logging
+from ckanext.onboarding_theodoro_bertol.views.home import home
 
+log = logging.getLogger(__name__)
 
 class OnboardingTheodoroBertolPlugin(plugins.SingletonPlugin):
     plugins.implements(plugins.IConfigurer)
+    plugins.implements(plugins.IBlueprint)
     
-
     # IConfigurer
-
     def update_config(self, config_):
+        log.info("OnboardingTheodoroBertolPlugin: update_config called")
         toolkit.add_template_directory(config_, "templates")
         toolkit.add_public_directory(config_, "public")
         toolkit.add_resource("assets", "onboarding_theodoro_bertol")
-
+        log.info("OnboardingTheodoroBertolPlugin: template and asset directories configured")
     
+    # IBlueprint
+    def get_blueprint(self):
+        log.info("OnboardingTheodoroBertolPlugin: get_blueprint called")
+        return [home]
---
+++ ckanext/onboarding_theodoro_bertol/views/home.py
@@ -0,0 +1,13 @@
+from flask import Blueprint
+import ckan.lib.base as base
+import logging
+
+log = logging.getLogger(__name__)
+
+home = Blueprint("onboarding_home", __name__)
+
+def my_new_route():
+    log.info("my_new_route view called!")
+    return base.render('home/about.html', extra_vars={})
+
+home.add_url_rule("/my-new-route", view_func=my_new_route)
